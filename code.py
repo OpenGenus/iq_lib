6d6198885b61b50f0e51db3a74977eb3e1f62ba7
Nisarg Shah <nisshah1499@gmail.com>
2019-07-20 22:51:31 +0530

    Added main function along with --output

--- code.py
+++ code.py
@@ -1,4 +1,19 @@
+import argparse
 import webbrowser
 
 def open_link(url):
     webbrowser.open_new_tab(url)
+
+def main(*args):
+    if len(args) == 0:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--open", help="Open the url")
+        args = parser.parse_args()
+        openTerm = args.open
+    else:
+        openTerm = args[0]
+
+    open_link(openTerm)
+
+if __name__ == "__main__":
+    main()
